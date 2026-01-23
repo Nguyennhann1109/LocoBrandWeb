353ee74d0b05bf2301b784d7f7dad6083846443b
Nguyennhann1109 <nguyennhann1109@gmail.com>
2026-01-23 16:11:00 +0700

    khoi tao

---
+++ forthehood/forthehood/src/test/java/com/example/forthehood/ForthehoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forthehood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForthehoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
